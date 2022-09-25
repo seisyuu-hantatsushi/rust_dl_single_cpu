c09a0ee7c2428b80ddd46fce51dec3a669cb1114
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2022-09-25 19:06:43 +0900

    apply Tensor structor

--- deep-learning/src/activator.rs
+++ deep-learning/src/activator.rs
@@ -1,18 +1,74 @@
 use std::ops;
 use num::Float;
 use linear_transform::matrix::MatrixMxN;
+use linear_transform::tensor::tensor_base::Tensor;
 
-pub fn sigmoid<T: Float+ops::AddAssign+ops::MulAssign>(m: &MatrixMxN<T>) -> MatrixMxN<T> {
-    let (c,r) = m.shape();
-    let one:T = num::one();
-    let e = one.exp();
-    let minus_one = one.neg();
-    let b:Vec<T> = m.buffer().iter().map(|v| one/(one+e.powf(minus_one*(*v)))).collect();
-    MatrixMxN::<T>::from_array(c,r,&b.into_boxed_slice())
+pub trait Activator<T> {
+    fn sigmoid(&self) -> T;
+    fn relu(&self) -> T;
 }
 
-pub fn relu<T: num::Float+ops::AddAssign+ops::MulAssign>(m: &MatrixMxN<T>) -> MatrixMxN<T> {
-    let (c,r) = m.shape();
-    let b:Vec<T> = m.buffer().iter().map(|v| if *v > num::zero() {*v} else {num::zero()}).collect();
-    MatrixMxN::from_array(c, r, &b.into_boxed_slice())
+impl<T> Activator<Tensor<T>> for Tensor<T>
+where T:num::Float+Clone {
+    fn sigmoid(&self) -> Tensor<T> {
+	let one:T = num::one();
+	let e = one.exp();
+	let minus_one = one.neg();
+	let b:Vec<T> = self.buffer().iter().map(|v| one/(one+e.powf(minus_one*(*v)))).collect();
+	Tensor::from_vector(self.shape().to_vec(), b)
+    }
+
+    fn relu(&self) -> Tensor<T> {
+	let b:Vec<T> = self.buffer().iter().map(|v| if *v > num::zero() {*v} else {num::zero()}).collect();
+	Tensor::from_vector(self.shape().to_vec(), b)
+    }
+}
+
+impl<T> Activator<MatrixMxN<T>> for MatrixMxN<T>
+    where T: num::Float+ops::AddAssign+ops::MulAssign {
+
+    fn sigmoid(&self) -> MatrixMxN<T> {
+	let (c,r) = self.shape();
+	let one:T = num::one();
+	let e = one.exp();
+	let minus_one = one.neg();
+	let b:Vec<T> = self.buffer().iter().map(|v| one/(one+e.powf(minus_one*(*v)))).collect();
+	MatrixMxN::<T>::from_array(c,r,&b.into_boxed_slice())
+    }
+
+    fn relu(&self) -> MatrixMxN<T> {
+	let (c,r) = self.shape();
+	let b:Vec<T> = self.buffer().iter().map(|v| if *v > num::zero() {*v} else {num::zero()}).collect();
+	MatrixMxN::from_array(c, r, &b.into_boxed_slice())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{f32,f64};
+    #[test]
+    fn activator_tensor_test () {
+	let v1 = Tensor::<f64>::from_array(&[1, 3], &[-1.0,1.0,2.0]);
+	let v2 = v1.sigmoid();
+	let v3 = Tensor::<f64>::from_array(&[1, 3], &[1.0/(1.0+f64::consts::E.powf(1.0)),1.0/(1.0+f64::consts::E.powf(-1.0)),1.0/(1.0+f64::consts::E.powf(-2.0))]);
+	assert_eq!(v2,v3);
+
+	let v4 = Tensor::<f64>::sigmoid(&v1);
+	assert_eq!(v2,v4);
+
+	let v5 = v1.relu();
+	assert_eq!(v5, Tensor::<f64>::from_array(&[1, 3], &[0.0, 1.0, 2.0]));
+    }
+
+    #[test]
+    fn activator_matrix_test() {
+	let v1 = MatrixMxN::from_array(1, 3, &[-1.0,1.0,2.0]);
+	let v2 = MatrixMxN::sigmoid(&v1);
+	let v3 = MatrixMxN::from_array(1, 3, &[1.0/(1.0+f64::consts::E.powf(1.0)),1.0/(1.0+f64::consts::E.powf(-1.0)),1.0/(1.0+f64::consts::E.powf(-2.0))]);
+	assert_eq!(v2,v3);
+
+	let v4 = v1.relu();
+	assert_eq!(v4, MatrixMxN::from_array(1, 3, &[0.0, 1.0, 2.0]));
+    }
 }
--- deep-learning/src/lib.rs
+++ deep-learning/src/lib.rs
@@ -14,17 +14,6 @@ mod tests {
     use gradient::*;
 
     #[test]
-    fn activator_test() {
-	let v1 = MatrixMxN::from_array(1, 3, &[-1.0,1.0,2.0]);
-	let v2 = sigmoid(&v1);
-	let v3 = MatrixMxN::from_array(1, 3, &[1.0/(1.0+f64::consts::E.powf(1.0)),1.0/(1.0+f64::consts::E.powf(-1.0)),1.0/(1.0+f64::consts::E.powf(-2.0))]);
-	assert_eq!(v2,v3);
-
-	let v4 = relu(&v1);
-	assert_eq!(v4, MatrixMxN::from_array(1, 3, &[0.0, 1.0, 2.0]));
-    }
-
-    #[test]
     fn network_test() {
 	{
 	    let input_data:Vec<Vec<f32>> = vec![vec![1.0, 0.5]];
@@ -42,9 +31,9 @@ mod tests {
 	    println!("{}", w2);
 
 	    let a1 = x * w1 + b1;
-	    let z1 = sigmoid(&a1);
+	    let z1 = a1.sigmoid();
 	    let a2 = z1 * w2 + b2;
-	    let z2 = sigmoid(&a2);
+	    let z2 = a2.sigmoid();
 	    let a3 = z2 * w3 + b3;
 	    let y = matrix_mxn::identity(a3);
 
@@ -60,24 +49,4 @@ mod tests {
 	    assert!((1.0-s).abs() < 1e-6);
 	}
    }
-    #[test]
-    fn loss_functions_test() {
-	let t = MatrixMxN::<f32>::from_array(1, 10, &[0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
-	let y = MatrixMxN::<f32>::from_array(1, 10, &[0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]);
-	let e = sum_squarted_error(&y,&t);
-	assert!((0.0975-e).abs() < 1e-6);
-
-	let y = MatrixMxN::<f32>::from_array(1, 10, &[0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]);
-	let e = sum_squarted_error(&y,&t);
-	assert!((0.5975-e).abs() < 1e-6);
-
-	let y = MatrixMxN::<f32>::from_array(1, 10, &[0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]);
-	let e = cross_entropy_error(&y,&t);
-	assert!((0.510825457-e).abs() < 1e-6);
-
-	let y = MatrixMxN::<f32>::from_array(1, 10, &[0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]);
-	let e = cross_entropy_error(&y,&t);
-	assert!((2.302584092-e).abs() < 1e-6);
-    }
-
 }
