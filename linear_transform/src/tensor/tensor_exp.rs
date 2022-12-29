d51b2c2f32555564c64551e8874b996c7eca599f
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2022-12-29 16:09:29 +0900

    append sigmod function.

--- linear_transform/src/tensor/tensor_exp.rs
+++ linear_transform/src/tensor/tensor_exp.rs
@@ -9,3 +9,13 @@ where T:num::Float+Clone+Copy {
 	Tensor::from_vector(self.shape().to_vec(), v)
     }
 }
+
+impl<T> Tensor<T>
+where T:num::Float+Clone+Copy {
+    pub fn sigmoid(&self) -> Tensor<T> {
+	let one:T = num::one();
+	let b:&[T] = self.buffer();
+	let v = b.iter().map(|b| one/(one - b.exp())).collect::<Vec<T>>();
+	Tensor::from_vector(self.shape().to_vec(), v)
+    }
+}
