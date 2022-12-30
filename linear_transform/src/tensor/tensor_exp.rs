40d30a2d3fc170d2ca52a8ff2058e0ac284eced8
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2022-12-30 10:50:18 +0900

    Fixed calculation of sigmoid function.

--- linear_transform/src/tensor/tensor_exp.rs
+++ linear_transform/src/tensor/tensor_exp.rs
@@ -15,7 +15,10 @@ where T:num::Float+Clone+Copy {
     pub fn sigmoid(&self) -> Tensor<T> {
 	let one:T = num::one();
 	let b:&[T] = self.buffer();
-	let v = b.iter().map(|b| one/(one - b.exp())).collect::<Vec<T>>();
+	let v = b.iter().map(|&b| {
+	    let neg_b:T = (num::zero::<T>() - one) * b;
+	    one/(one + (neg_b.exp()))
+	}).collect::<Vec<T>>();
 	Tensor::from_vector(self.shape().to_vec(), v)
     }
 }
