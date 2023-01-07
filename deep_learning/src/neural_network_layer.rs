05f88bf9f17fab4a5901e020d9716636db1763e0
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2023-01-07 15:16:17 +0900

    Introduce the layer function.

---
+++ deep_learning/src/neural_network_layer.rs
@@ -0,0 +1,118 @@
+/* -*- tab-width:4 -*- */
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use rand::prelude::*;
+use rand::SeedableRng;
+use rand_distr::{Normal,Distribution};
+use rand_pcg::Pcg64;
+
+use linear_transform::tensor::Tensor;
+use crate::neural_network::NeuralNetwork;
+use crate::neuron::{NeuronPrimType,NNNeuron};
+
+pub struct Linear<T>
+where T: NeuronPrimType<T> {
+	name: String,
+	enable_bias: bool,
+	out_size: usize,
+	w: Option<NNNeuron<T>>,
+	b: Option<NNNeuron<T>>,
+	output: Option<NNNeuron<T>>
+}
+
+pub type NNLinear<T> = RefCell<Linear<T>>;
+
+pub enum NNLayer<T>
+where T: NeuronPrimType<T> {
+    Linear(NNLinear<T>)
+}
+
+impl<T> NeuralNetwork<T>
+where T:NeuronPrimType<T> {
+
+	pub fn create_linear_layer(&mut self,
+							   name: &str,
+							   out_size:usize,
+							   enable_bias:bool) -> NNLayer<T> {
+		NNLayer::Linear(
+			RefCell::new(
+				Linear {
+					name: name.to_string(),
+					enable_bias: enable_bias,
+					out_size: out_size,
+					w: None,
+					b: None,
+					output: None
+				}
+			)
+		)
+	}
+
+	pub fn layer_set_inputs(&mut self, layer:&NNLayer<T>, inputs:Vec<NNNeuron<T>>) -> Vec<NNNeuron<T>> {
+
+		match layer {
+			NNLayer::<T>::Linear(l) => {
+				let mut ns = vec!();
+				let mut borrowed_l = l.borrow_mut();
+				if let None = borrowed_l.w {
+					let mut rng = Pcg64::from_entropy();
+					let normal_dist = Normal::new(0.0,1.0).unwrap_or_else(|e| panic!("{} {}:{}", e.to_string(), file!(), line!()));
+					let in_size = inputs[0].borrow().shape()[1];
+					let w_shape = vec![in_size, borrowed_l.out_size];
+					let ws = (0..(w_shape[0]*w_shape[1])).map(|_| {
+						let w:T = num::FromPrimitive::from_f64(normal_dist
+															   .sample(&mut rng))
+							.unwrap_or_else(||
+											panic!("failed to get normal dist value"));
+						let l:T = num::FromPrimitive::from_usize(in_size)
+							.unwrap_or_else(||
+											panic!("failed to get length"));
+						let rate = (num::one::<T>()/l).sqrt();
+						w * rate
+					}).collect();
+					let label = borrowed_l.name.clone() + "_w";
+					let wn = self.create_neuron(&label, Tensor::<T>::from_vector(w_shape,ws));
+					borrowed_l.w = Some(Rc::clone(&wn));
+					let output = if borrowed_l.enable_bias {
+						let label = borrowed_l.name.clone() + "_b";
+						let b = self.create_neuron(&label,Tensor::<T>::zero(&[1,1]));
+						let output = self.affine(Rc::clone(&inputs[0]), wn, Some(Rc::clone(&b)));
+						borrowed_l.b = Some(b);
+						output
+					}
+					else {
+						self.affine(Rc::clone(&inputs[0]), wn, None)
+					};
+					borrowed_l.output = Some(Rc::clone(&output));
+					ns.push(output);
+				}
+				else {
+					panic!("already set input variable");
+				}
+				ns
+			},
+			_ => vec!()
+		}
+	}
+}
+
+impl<T> NNLayer<T>
+where T:NeuronPrimType<T> {
+	pub fn get_layer_param(&self) -> Vec<NNNeuron<T>> {
+		match self {
+			NNLayer::<T>::Linear(l) => {
+				let mut ns:Vec<NNNeuron<T>> = vec!();
+				let borrowed_l = l.borrow();
+				if let Some(ref w) = borrowed_l.w {
+					ns.push(Rc::clone(w));
+				};
+				if let Some(ref b) = borrowed_l.b {
+					ns.push(Rc::clone(b));
+				}
+				ns
+			}
+		}
+	}
+}
---
+++ deep_learning/tests/nn_layer.rs
@@ -0,0 +1,124 @@
+/* -*- tab-width:4 -*- */
+
+use std::fmt::Display;
+use std::rc::Rc;
+use std::error::Error;
+
+use rand::SeedableRng;
+use rand_distr::{Uniform,Distribution};
+use rand_pcg::Pcg64;
+
+use deep_learning::neural_network::NeuralNetwork;
+use linear_transform::Tensor;
+
+use plotters::prelude::full_palette::*;
+use plotters::prelude::{Circle,BitMapBackend,LineSeries,ChartBuilder,PathElement,SeriesLabelPosition};
+use plotters::drawing::IntoDrawingArea;
+use plotters::style::{IntoFont,Color};
+
+#[derive(Debug)]
+enum MyError {
+    StringMsg(String)
+}
+
+impl Display for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+	use self::MyError::*;
+	match self {
+	    StringMsg(s) => write!(f, "{}", s)
+	}
+    }
+}
+
+impl Error for MyError {}
+
+#[test]
+fn linear_layer_test() ->  Result<(),Box<dyn std::error::Error>> {
+
+	let mut rng = Pcg64::from_entropy();
+	let mut nn = NeuralNetwork::<f64>::new();
+	let layer1 = nn.create_linear_layer("LL1", 10, true);
+	let layer2 = nn.create_linear_layer("LL2",  1, true);
+
+	let uniform_dist = Uniform::new(0.0,1.0);
+	let xs:Vec<f64> = (0..100).map(|_| uniform_dist.sample(&mut rng)).collect();
+	let uniform_dist = Uniform::new(-0.5,0.5);
+	let ys:Vec<f64> = xs.iter().map(|&x| (2.0*std::f64::consts::PI*x).sin()+uniform_dist.sample(&mut rng)).collect();
+	let x = nn.create_constant("x", Tensor::<f64>::from_vector(vec![100,1], xs));
+	let y = nn.create_constant("y", Tensor::<f64>::from_vector(vec![100,1], ys));
+	let ll1_output = nn.layer_set_inputs(&layer1, vec![Rc::clone(&x)]);
+	let act_output = nn.sigmoid(Rc::clone(&ll1_output[0]));
+	let pred_y     = nn.layer_set_inputs(&layer2, vec![Rc::clone(&act_output)]);
+	let loss       = nn.mean_square_error(Rc::clone(&y), Rc::clone(&pred_y[0]));
+	let learning_rate = 0.2;
+
+	nn.backward_propagating(0)?;
+
+	nn.make_dot_graph(0,"2layer_graph.dot")?;
+
+	for i in 0..10000 {
+
+		for l in vec![&layer1, &layer2] {
+			for param in l.get_layer_param() {
+				let feedback = if let Some(ref g) = param.borrow().ref_grad() {
+					g.borrow().ref_signal().scale(learning_rate)
+				}
+				else {
+					return Err(Box::new(MyError::StringMsg("param does not have grad".to_string())));
+				};
+				let update = param.borrow().ref_signal() - feedback;
+				param.borrow_mut().assign(update);
+			}
+		}
+
+		if i % 1000 == 0 {
+			println!("{}", loss.borrow());
+		}
+
+		nn.forward_propagating(0)?;
+		nn.forward_propagating(1)?;
+
+	}
+
+	{
+		let xt = x.borrow().ref_signal().clone();
+		let yt = y.borrow().ref_signal().clone();
+		let data_points: Vec<(&f64, &f64)> =
+			xt.buffer().iter().zip(yt.buffer().iter()).collect();
+
+		let pred_xs:Vec<f64> = (0..100).map(|i| (i as f64)/100.0).collect();
+		let pred_xt = Tensor::<f64>::from_vector(vec![100,1], pred_xs);
+		x.borrow_mut().assign(pred_xt.clone());
+		nn.forward_propagating(0)?;
+		let pred_yt = y.borrow().ref_signal().clone();
+		let pred_points: Vec<(&f64, &f64)> =
+			pred_xt.buffer().iter().zip(pred_yt.buffer().iter()).collect();
+
+		let render_backend = BitMapBackend::new("simple_nn_layer.png", (800,600)).into_drawing_area();
+		render_backend.fill(&WHITE)?;
+		let mut data_points_chart_builder = ChartBuilder::on(&render_backend)
+			.caption("simple neural network (layer)", ("sans-serif", 50).into_font())
+			.margin(10)
+			.x_label_area_size(30)
+			.y_label_area_size(30)
+			.build_cartesian_2d(0.0f32..1.0f32, -2.0f32..2.0f32)?;
+		data_points_chart_builder.configure_mesh().disable_x_mesh().disable_y_mesh().draw()?;
+
+		data_points_chart_builder
+			.draw_series(data_points.iter().map(|(x,y)| Circle::new((**x as f32,**y as f32), 2, GREEN.filled())))?;
+
+		data_points_chart_builder
+			.draw_series(LineSeries::new(pred_points.iter().map(|(x, y)| (**x as f32, **y as f32)).collect::<Vec<(f32,f32)>>(),&RED))?
+			.label(format!("prediction"))
+			.legend(|(x, y)| PathElement::new(vec![(x, y), (x+20, y)], &RED));
+
+		data_points_chart_builder
+			.configure_series_labels()
+			.position(SeriesLabelPosition::UpperLeft)
+			.background_style(&WHITE.mix(0.8))
+			.border_style(&BLACK)
+			.draw()?;
+		render_backend.present()?;
+	}
+	Ok(())
+}
